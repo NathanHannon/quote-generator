7d62f35c9b7065945f59bc05cb5d138fd27fe4d7
Nathan Hannon <nathan@hannon.me>
2020-10-08 21:00:11 -0400

    added loader

--- script.js
+++ script.js
@@ -3,9 +3,23 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+// Hide Loading
+function complete() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 // Get Quote from API
 async function getQuote() {
+    loading();
     const proxyUrl = 'https://arcane-lake-58290.herokuapp.com/';
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
@@ -26,6 +40,8 @@ async function getQuote() {
             quoteText.classList.add('long-quote');
         }
         quoteText.innerText = data.quoteText;
+        // Stop Loader, Show quote
+        complete();
     } catch (error) {
         getQuote();
     }
